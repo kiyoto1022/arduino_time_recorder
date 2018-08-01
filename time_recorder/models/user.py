fb8dc72e07646d5112454e5134114e169075ba1e
kiyoto1022 <kiyoto1022@gmail.com>
2018-08-01 20:11:11 +0900

    リファクタリング

--- time_recorder/models/user.py
+++ time_recorder/models/user.py
@@ -11,31 +11,39 @@ class User:
         self.out_time = None
         self.recognizer = cv2.face.LBPHFaceRecognizer_create()
 
+    def learn(self, images, labels):
+        self.recognizer.train(images, np.array(labels))
+
     def is_punch_in(self):
         if self.in_time is not None:
             return True
         return False
 
     def punch_in(self):
+        if self.is_punch_in():
+            print "{} is already punch in".format(self.id)
+            return
+
         self.in_time = datetime.datetime.today()
+        print "{} is punch in : {}".format(self.id, self.in_time)
 
-    def punch_out(self):
+    def punch_out(self, face):
+        self.identification(face)
         self.out_time = datetime.datetime.today()
+        print "{} is punch out : {}".format(self.name, self.in_time)
+        print "working time {} second".format(self.working_time_sec())
 
-    def working_time_sec(self):
-        total_sec = (self.out_time - self.in_time).total_seconds()
-        self.in_time = None
-        self.out_time = None
-        return total_sec
-
-    def learn(self, images, labels):
-        self.recognizer.train(images, np.array(labels))
-
-    def identification(self, face):
+    def __identification(self, face):
         label, confidence = self.recognizer.predict(face)
         print("Predicted Label: {}, Confidence: {}".format(label, confidence))
 
         if confidence > 90:
-            print "It is my face."
+            print "It is my face"
         else:
             print "It is not my face"
+
+    def __working_time_sec(self):
+        total_sec = (self.out_time - self.in_time).total_seconds()
+        self.in_time = None
+        self.out_time = None
+        return total_sec
--- time_recorder/time_record.py
+++ time_recorder/time_record.py
@@ -18,65 +18,6 @@ faceCascade = cv2.CascadeClassifier(cascadePath)
 users = Users()
 
 
-def get_face_list_and_label(path):
-    face_list = []
-    labels = []
-    for image in os.listdir(path):
-        face = detect_face(os.path.join(path, image))
-        if face is not None:
-            face_list.append(face)
-            labels.append(int(os.path.splitext(image)[0]))
-
-    return face_list, labels
-
-
-def detect_face(path):
-    face_image = np.array(Image.open(path).convert('L'), 'uint8')
-
-    # Detect only one face
-    for (x, y, w, h) in faceCascade.detectMultiScale(face_image):
-        return cv2.resize(face_image[y: y + h, x: x + w], (200, 200), interpolation=cv2.INTER_LINEAR)
-
-    # Face undetected
-    print "Face undetected"
-    return None
-
-
-def shot():
-    # FIXME: Is it necessary to repeat twice?
-    ret, frame = cap.read()
-    cv2.imwrite("./punch_out_user.jpg", frame)
-    time.sleep(WAIT_FOR_SAVING_COMPLETION)
-    return "./punch_out_user.jpg"
-
-
-def punch_out(user):
-    if not user.is_punch_in():
-        print "{} is not punch in.".format(user.id)
-        return
-
-    face = detect_face(shot())
-    if face is None:
-        print "Face could not be detected."
-        return
-
-    # log only
-    user.identification(face)
-
-    user.punch_out()
-    print "{} is punch out : {}.".format(user.name, user.in_time)
-    print "working time {} second.".format(user.working_time_sec())
-
-
-def punch_in(user):
-    if user.is_punch_in():
-        print "{} is already punch in.".format(user.id)
-        return
-
-    user.punch_in()
-    print "{} is punch in : {}.".format(user.id, user.in_time)
-
-
 def training():
     for user in users.list():
         images, labels = get_face_list_and_label("./data/users_face/{}".format(user.name))
@@ -89,20 +30,60 @@ def standby_camera():
     cv2.imwrite("test_shot.jpg", frame)
 
 
+def detect_face(path):
+    face_image = np.array(Image.open(path).convert('L'), 'uint8')
+
+    # Detect only one face
+    for (x, y, w, h) in faceCascade.detectMultiScale(face_image):
+        return cv2.resize(face_image[y: y + h, x: x + w], (200, 200), interpolation=cv2.INTER_LINEAR)
+
+    print "Face undetected"
+    return None
+
+
+def get_face_list_and_label(path):
+    face_list = []
+    labels = []
+    for image in os.listdir(path):
+        face = detect_face(os.path.join(path, image))
+        if face is not None:
+            face_list.append(face)
+            labels.append(int(os.path.splitext(image)[0]))
+
+    return face_list, labels
+
+
+def shot():
+    while True:
+        ret, frame = cap.read()
+        cv2.imwrite("./punch_out_user.jpg", frame)
+        time.sleep(WAIT_FOR_SAVING_COMPLETION)
+
+        face = detect_face("./punch_out_user.jpg")
+        if face is None:
+            print "Face could not be detected"
+            continue
+        return face
+
+
 def receiving(ser):
     message = Message(ser.readline())
     if not message.is_receive():
         return
 
     if not users.is_member(message.user_id):
-        print "{} is not member.".format(message.user_id)
+        print "{} is not member".format(message.user_id)
         return
 
+    user = users.get(message.user_id)
     if message.is_punch_in():
-        punch_in(users.get(message.user_id))
+        user.punch_in()
 
     if message.is_punch_out():
-        punch_out(users.get(message.user_id))
+        if not user.is_punch_in():
+            print "{} is not punch in".format(user.id)
+            return
+        user.punch_out(shot())
 
 
 def main():
